27b6d56fec904047e8cd6afcdbae6d83807d472b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-02-24 19:13:39 -0500

    fixed compile error

--- common/src/main/java/org/gtreimagined/gtcore/mixin/NearestAttackableTargetMixin.java
+++ common/src/main/java/org/gtreimagined/gtcore/mixin/NearestAttackableTargetMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import org.gtreimagined.gtcore.events.GTCommonEvents;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
 import java.util.function.Predicate;
 
 @Mixin(NearestAttackableTargetGoal.class)
