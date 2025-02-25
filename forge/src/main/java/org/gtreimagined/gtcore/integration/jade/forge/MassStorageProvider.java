b3b7ea2a36b4360f73e5c4eb3b6ee408350a06d3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-02-24 19:07:05 -0500

    improved MassStorageProvider

--- forge/src/main/java/org/gtreimagined/gtcore/integration/jade/forge/MassStorageProvider.java
+++ forge/src/main/java/org/gtreimagined/gtcore/integration/jade/forge/MassStorageProvider.java
@@ -4,12 +4,19 @@ import mcp.mobius.waila.api.BlockAccessor;
 import mcp.mobius.waila.api.IComponentProvider;
 import mcp.mobius.waila.api.ITooltip;
 import mcp.mobius.waila.api.config.IPluginConfig;
+import mcp.mobius.waila.api.ui.IElement;
+import muramasa.antimatter.gui.SlotType;
 import muramasa.antimatter.util.Utils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.ItemStack;
 import org.gtreimagined.gtcore.blockentity.BlockEntityMassStorage;
 import org.gtreimagined.gtcore.data.SlotTypes;
 import snownee.jade.Jade;
 import snownee.jade.VanillaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MassStorageProvider implements IComponentProvider {
     public static final MassStorageProvider INSTANCE = new MassStorageProvider();
@@ -18,11 +25,23 @@ public class MassStorageProvider implements IComponentProvider {
     public void appendTooltip(ITooltip iTooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
         if (iPluginConfig.get(JadePlugin.MASS_STORAGE) && blockAccessor.getBlockEntity() instanceof BlockEntityMassStorage massStorage){
             var handler = massStorage.itemHandler.map(i -> i.getHandler(SlotTypes.UNLIMITED)).orElse(null);
-            if (handler != null) {
+            var displayHandler = massStorage.itemHandler.map(i -> i.getHandler(SlotType.DISPLAY)).orElse(null);
+            if (handler != null && displayHandler != null) {
                 ItemStack stack = handler.getItem(0);
+                boolean displayItem = false;
+                if (stack.isEmpty()) {
+                    stack = displayHandler.getItem(0);
+                    displayItem = true;
+                }
                 if (!stack.isEmpty()){
-                    iTooltip.add(iTooltip.getElementHelper().item(stack, 1.0f, (stack.getCount() < 10000 ? String.valueOf(stack.getCount()) : VanillaPlugin.getDisplayHelper().humanReadableNumber(stack.getCount(), "", false))).tag(JadePlugin.MASS_STORAGE));
-                    iTooltip.add(iTooltip.getElementHelper().text(stack.getHoverName()).tag(VanillaPlugin.INVENTORY).message(null));
+                    ItemStack copy = stack.copy();
+                    copy.setCount(1);
+                    var helper = iTooltip.getElementHelper();
+                    List<IElement> elements = new ArrayList<>();
+                    elements.add(Jade.smallItem(helper, copy).tag(JadePlugin.MASS_STORAGE));
+                    String text = displayItem ? "" : (stack.getCount() < 10000 ? String.valueOf(stack.getCount()) : VanillaPlugin.getDisplayHelper().humanReadableNumber(stack.getCount(), "", false)) + "× ";
+                    elements.add(helper.text(Utils.literal(text).append(stack.getHoverName())).tag(JadePlugin.MASS_STORAGE).message(null));
+                    iTooltip.add(elements);
                 }
             }
         }
