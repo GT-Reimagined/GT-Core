95fa6d4f8370a6566052f144c981014bf19c46d7
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-09 12:53:10 -0400

    fall back to rods for drum recipes if material does not have long rods

--- src/main/java/org/gtreimagined/gtcore/loader/crafting/MachineRecipes.java
+++ src/main/java/org/gtreimagined/gtcore/loader/crafting/MachineRecipes.java
@@ -4,6 +4,8 @@ import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.datagen.providers.AntimatterRecipeProvider;
 import muramasa.antimatter.material.Material;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.Tags;
 import org.gtreimagined.gtcore.GTCore;
 import org.gtreimagined.gtcore.machine.DrumMachine;
@@ -14,16 +16,16 @@ import java.util.function.Consumer;
 import static com.google.common.collect.ImmutableMap.of;
 import static muramasa.antimatter.data.AntimatterDefaultTools.HAMMER;
 import static muramasa.antimatter.data.AntimatterDefaultTools.WRENCH;
-import static muramasa.antimatter.data.AntimatterMaterialTypes.PLATE;
-import static muramasa.antimatter.data.AntimatterMaterialTypes.ROD_LONG;
+import static muramasa.antimatter.data.AntimatterMaterialTypes.*;
 import static muramasa.antimatter.machine.Tier.NONE;
 
 public class MachineRecipes {
     public static void initRecipes(Consumer<FinishedRecipe> output, AntimatterRecipeProvider provider){
         AntimatterAPI.all(DrumMachine.class).forEach(d -> {
             Material m = d.getMaterial();
-            if (m.has(PLATE) && m.has(ROD_LONG)){
-                provider.addItemRecipe(output, GTCore.ID, "", "machines", d.getItem(NONE), of('H', HAMMER.getTag(), 'R', ROD_LONG.getMaterialTag(m), 'P', PLATE.getMaterialTag(m)), " H ", "PRP", "PRP");
+            if (m.has(PLATE) && (m.has(ROD_LONG) || m.has(ROD))){
+                TagKey<Item> rod = m.has(ROD_LONG) ? ROD_LONG.getMaterialTag(m) : ROD.getMaterialTag(m);
+                provider.addItemRecipe(output, GTCore.ID, "", "machines", d.getItem(NONE), of('H', HAMMER.getTag(), 'R', rod, 'P', PLATE.getMaterialTag(m)), " H ", "PRP", "PRP");
             }
         });
         AntimatterAPI.all(HopperMachine.class).forEach(h -> {
