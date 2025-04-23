e8afbbb89cb1f859f63e8a77ad9f3017111502ab
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-04-23 15:02:03 -0400

    added alternate stone textures as a resource pack

--- src/main/java/org/gtreimagined/gtcore/proxy/ClientHandler.java
+++ src/main/java/org/gtreimagined/gtcore/proxy/ClientHandler.java
@@ -8,6 +8,8 @@ import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.gtreimagined.gtcore.GTCore;
 import org.gtreimagined.gtcore.block.BlockCasing;
 import org.gtreimagined.gtcore.block.BlockGTHopper;
 import org.gtreimagined.gtcore.block.BlockMaterialChest;
@@ -23,6 +25,10 @@ import org.gtreimagined.gtcore.machine.MassStorageMachine;
 import org.gtreimagined.gtlib.GTAPI;
 import org.gtreimagined.gtlib.client.ModelUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.Consumer;
 
 public class ClientHandler {
@@ -51,6 +57,7 @@ public class ClientHandler {
             ModelUtils.setRenderLayer(GTCoreBlocks.RUBBER_TRAPDOOR, RenderType.cutout());
             ModelUtils.setRenderLayer(GTCoreBlocks.RUBBER_DOOR, RenderType.cutout());
             ModelUtils.setRenderLayer(GTCoreBlocks.SAP_BAG, RenderType.cutout());
+            writeResourcePack("Alternate-Stone-Textures", "alternate-stone-textures");
         });
     }
 
@@ -61,5 +68,28 @@ public class ClientHandler {
 
         spriteFunction.accept(MaterialChestRenderer.MATERIAL_CHEST_BASE);
         spriteFunction.accept(MaterialChestRenderer.MATERIAL_CHEST_OVERLAY);
+    }
+
+    private static void writeResourcePack(String writeName, String readName){
+        File dir = new File(FMLPaths.CONFIGDIR.get().getParent().toFile(), "resourcepacks");
+        File target = new File(dir, writeName + ".zip");
+
+
+        //if(!target.exists())
+        try {
+            dir.mkdirs();
+            InputStream in = GTCore.class.getResourceAsStream("/assets/" + GTCore.ID + "/" + readName + ".zip");
+            FileOutputStream out = new FileOutputStream(target);
+
+            byte[] buf = new byte[16384];
+            int len = 0;
+            while((len = in.read(buf)) > 0)
+                out.write(buf, 0, len);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
