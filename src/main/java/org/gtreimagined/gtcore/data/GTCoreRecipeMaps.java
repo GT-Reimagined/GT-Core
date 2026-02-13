09e5311077d140a856fe05e16bf1517c63cf3a19
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-02-13 16:08:10 -0500

    set info renderer of mortar to empty renderer to remove empty space

--- src/main/java/org/gtreimagined/gtcore/data/GTCoreRecipeMaps.java
+++ src/main/java/org/gtreimagined/gtcore/data/GTCoreRecipeMaps.java
@@ -1,9 +1,8 @@
 package org.gtreimagined.gtcore.data;
 
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.gtreimagined.gtcore.GTCore;
-import org.gtreimagined.gtcore.block.BlockMortar;
-import org.gtreimagined.gtlib.GTAPI;
-import org.gtreimagined.gtlib.integration.xei.GTLibXEIPlugin;
+import org.gtreimagined.gtlib.integration.xei.renderer.InfoRenderers;
 import org.gtreimagined.gtlib.recipe.map.RecipeBuilder;
 import org.gtreimagined.gtlib.recipe.map.RecipeMap;
 
@@ -11,5 +10,8 @@ public class GTCoreRecipeMaps {
     public static final RecipeMap<RecipeBuilder> MORTAR = new RecipeMap<>(GTCore.ID, "mortar", new RecipeBuilder());
     public static void init() {
         MORTAR.setIcon(() -> GTCoreBlocks.IRON_MORTAR).setGuiData(Guis.SIMPLE_DISPLAY);
+        if (FMLEnvironment.dist.isClient()){
+            MORTAR.setInfoRenderer(InfoRenderers.EMPTY_RENDERER);
+        }
     }
 }
