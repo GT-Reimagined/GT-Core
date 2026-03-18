562775037f70161ad865497f2541b5f762d5ac0f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-18 14:20:40 -0400

    changed UITextures to use builder

--- src/main/java/org/gtreimagined/gtcore/mui/GTCoreGuiTextures.java
+++ src/main/java/org/gtreimagined/gtcore/mui/GTCoreGuiTextures.java
@@ -6,9 +6,9 @@ import org.gtreimagined.gtcore.GTCore;
 
 public class GTCoreGuiTextures {
     public static final UITexture TRASH_CAN = UITexture.builder().location(GTCore.ID, "textures/gui/icons/trash_can.png").imageSize(64, 68).build();
-    public static final UITexture BRONZE_BACKGROUND = UITexture.fullImage(GTCore.ID, "textures/gui/background/bronze.png");
-    public static final UITexture STEEL_BACKGROUND = UITexture.fullImage(GTCore.ID, "textures/gui/background/steel.png");
-    public static final UITexture PRIMITIVE_BACKGROUND = UITexture.fullImage(GTCore.ID, "textures/gui/background/primitive.png");
+    public static final UITexture BRONZE_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/bronze.png").build();
+    public static final UITexture STEEL_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/steel.png").build();
+    public static final UITexture PRIMITIVE_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/primitive.png").build();
     public static final UITexture BLUEPRINT_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/blueprint.png").defaultColorType().build();
     public static final UITexture CRAFTING_OUTPUT_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/crafting_output.png").defaultColorType().build();
     public static final UITexture ELECTRIC_TOOL_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/electric_tool.png").defaultColorType().build();
@@ -16,12 +16,12 @@ public class GTCoreGuiTextures {
     public static final UITexture PARK_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/park.png").defaultColorType().build();
     public static final UITexture BLANK_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/blank.png").colorType(null).build();
 
-    public static final UITexture BRONZE_ITEM_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/item_bronze.png");
-    public static final UITexture STEEL_ITEM_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/item_steel.png");
-    public static final UITexture PRIMITIVE_ITEM_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/item_primitive.png");
-    public static final UITexture BRONZE_FLUID_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/fluid_bronze.png");
-    public static final UITexture STEEL_FLUID_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/fluid_steel.png");
-    public static final UITexture PRIMITIVE_FLUID_SLOT = UITexture.fullImage(GTCore.ID, "textures/gui/slots/fluid_primitive.png");
+    public static final UITexture BRONZE_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_bronze.png").build();
+    public static final UITexture STEEL_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_steel.png").build();
+    public static final UITexture PRIMITIVE_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_primitive.png").build();
+    public static final UITexture BRONZE_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_bronze.png").build();
+    public static final UITexture STEEL_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_steel.png").build();
+    public static final UITexture PRIMITIVE_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_primitive.png").build();
 
     public static final UITexture TO_INV_BUTTON = UITexture.builder().location(GTCore.ID, "textures/gui/button/to_inv.png").defaultColorType().build();
     public static final UITexture TO_PLAYER_BUTTON = UITexture.builder().location(GTCore.ID, "textures/gui/button/to_player.png").defaultColorType().build();
