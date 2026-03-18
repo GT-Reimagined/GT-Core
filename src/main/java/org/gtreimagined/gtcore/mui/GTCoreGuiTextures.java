8b3ab2ecd18fb02d9fc9e4bb76c614c8230b995b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-18 15:25:13 -0400

    fixed themes not working

--- src/main/java/org/gtreimagined/gtcore/mui/GTCoreGuiTextures.java
+++ src/main/java/org/gtreimagined/gtcore/mui/GTCoreGuiTextures.java
@@ -1,14 +1,25 @@
 package org.gtreimagined.gtcore.mui;
 
-import brachy.modularui.drawable.ColorType;
 import brachy.modularui.drawable.UITexture;
 import org.gtreimagined.gtcore.GTCore;
 
 public class GTCoreGuiTextures {
+    public static class IDs {
+        public static final String BRONZE_BACKGROUND = GTCore.ID + ":bronze_background";
+        public static final String STEEL_BACKGROUND = GTCore.ID + ":steel_background";
+        public static final String PRIMITIVE_BACKGROUND = GTCore.ID + ":primitive_background";
+        public static final String BRONZE_ITEM_SLOT = GTCore.ID + ":bronze_item_slot";
+        public static final String STEEL_ITEM_SLOT = GTCore.ID + ":steel_item_slot";
+        public static final String PRIMITIVE_ITEM_SLOT = GTCore.ID + ":primitive_item_slot";
+        public static final String BRONZE_FLUID_SLOT = GTCore.ID + ":bronze_fluid_slot";
+        public static final String STEEL_FLUID_SLOT = GTCore.ID + ":steel_fluid_slot";
+        public static final String PRIMITIVE_FLUID_SLOT = GTCore.ID + ":primitive_fluid_slot";
+    }
+
     public static final UITexture TRASH_CAN = UITexture.builder().location(GTCore.ID, "textures/gui/icons/trash_can.png").imageSize(64, 68).build();
-    public static final UITexture BRONZE_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/bronze.png").build();
-    public static final UITexture STEEL_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/steel.png").build();
-    public static final UITexture PRIMITIVE_BACKGROUND = UITexture.builder().location(GTCore.ID, "textures/gui/background/primitive.png").build();
+    public static final UITexture BRONZE_BACKGROUND = UITexture.builder().name(IDs.BRONZE_BACKGROUND).location(GTCore.ID, "textures/gui/background/bronze.png").build();
+    public static final UITexture STEEL_BACKGROUND = UITexture.builder().name(IDs.STEEL_BACKGROUND).location(GTCore.ID, "textures/gui/background/steel.png").build();
+    public static final UITexture PRIMITIVE_BACKGROUND = UITexture.builder().name(IDs.PRIMITIVE_BACKGROUND).location(GTCore.ID, "textures/gui/background/primitive.png").build();
     public static final UITexture BLUEPRINT_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/blueprint.png").defaultColorType().build();
     public static final UITexture CRAFTING_OUTPUT_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/crafting_output.png").defaultColorType().build();
     public static final UITexture ELECTRIC_TOOL_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/electric_tool.png").defaultColorType().build();
@@ -16,12 +27,12 @@ public class GTCoreGuiTextures {
     public static final UITexture PARK_SLOT_OVERLAY = UITexture.builder().location(GTCore.ID, "textures/gui/slots/overlay/park.png").defaultColorType().build();
     public static final UITexture BLANK_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/blank.png").colorType(null).build();
 
-    public static final UITexture BRONZE_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_bronze.png").build();
-    public static final UITexture STEEL_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_steel.png").build();
-    public static final UITexture PRIMITIVE_ITEM_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/item_primitive.png").build();
-    public static final UITexture BRONZE_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_bronze.png").build();
-    public static final UITexture STEEL_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_steel.png").build();
-    public static final UITexture PRIMITIVE_FLUID_SLOT = UITexture.builder().location(GTCore.ID, "textures/gui/slots/fluid_primitive.png").build();
+    public static final UITexture BRONZE_ITEM_SLOT = UITexture.builder().name(IDs.BRONZE_ITEM_SLOT).location(GTCore.ID, "textures/gui/slots/item_bronze.png").build();
+    public static final UITexture STEEL_ITEM_SLOT = UITexture.builder().name(IDs.STEEL_ITEM_SLOT).location(GTCore.ID, "textures/gui/slots/item_steel.png").build();
+    public static final UITexture PRIMITIVE_ITEM_SLOT = UITexture.builder().name(IDs.PRIMITIVE_ITEM_SLOT).location(GTCore.ID, "textures/gui/slots/item_primitive.png").build();
+    public static final UITexture BRONZE_FLUID_SLOT = UITexture.builder().name(IDs.BRONZE_FLUID_SLOT).location(GTCore.ID, "textures/gui/slots/fluid_bronze.png").build();
+    public static final UITexture STEEL_FLUID_SLOT = UITexture.builder().name(IDs.STEEL_FLUID_SLOT).location(GTCore.ID, "textures/gui/slots/fluid_steel.png").build();
+    public static final UITexture PRIMITIVE_FLUID_SLOT = UITexture.builder().name(IDs.PRIMITIVE_FLUID_SLOT).location(GTCore.ID, "textures/gui/slots/fluid_primitive.png").build();
 
     public static final UITexture TO_INV_BUTTON = UITexture.builder().location(GTCore.ID, "textures/gui/button/to_inv.png").defaultColorType().build();
     public static final UITexture TO_PLAYER_BUTTON = UITexture.builder().location(GTCore.ID, "textures/gui/button/to_player.png").defaultColorType().build();
--- src/main/java/org/gtreimagined/gtcore/mui/GTCoreThemes.java
+++ src/main/java/org/gtreimagined/gtcore/mui/GTCoreThemes.java
@@ -11,6 +11,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import org.gtreimagined.gtcore.GTCore;
+import org.gtreimagined.gtcore.mui.GTCoreGuiTextures.IDs;
 
 @EventBusSubscriber(modid = GTCore.ID, bus = Bus.FORGE)
 public class GTCoreThemes {
@@ -18,17 +19,17 @@ public class GTCoreThemes {
     public static final String STEEL_THEME_ID = GTCore.ID + ":steel";
     public static final String PRIMITIVE_THEME_ID = GTCore.ID + ":primitive";
     public static final ThemeBuilder<?> BRONZE_THEME = new ThemeBuilder<>(BRONZE_THEME_ID)
-            .background(IThemeApi.PANEL, GTCoreGuiTextures.BRONZE_BACKGROUND)
-            .background(IThemeApi.ITEM_SLOT, GTCoreGuiTextures.BRONZE_ITEM_SLOT)
-            .background(IThemeApi.FLUID_SLOT, GTCoreGuiTextures.BRONZE_FLUID_SLOT);
+            .background(IThemeApi.PANEL, IDs.BRONZE_BACKGROUND)
+            .background(IThemeApi.ITEM_SLOT, IDs.BRONZE_ITEM_SLOT)
+            .background(IThemeApi.FLUID_SLOT, IDs.BRONZE_FLUID_SLOT);
     public static final ThemeBuilder<?> STEEL_THEME = new ThemeBuilder<>(STEEL_THEME_ID)
-            .background(IThemeApi.PANEL, GTCoreGuiTextures.STEEL_BACKGROUND)
-            .background(IThemeApi.ITEM_SLOT, GTCoreGuiTextures.STEEL_ITEM_SLOT)
-            .background(IThemeApi.FLUID_SLOT, GTCoreGuiTextures.STEEL_FLUID_SLOT);
+            .background(IThemeApi.PANEL, IDs.STEEL_BACKGROUND)
+            .background(IThemeApi.ITEM_SLOT, IDs.STEEL_ITEM_SLOT)
+            .background(IThemeApi.FLUID_SLOT, IDs.STEEL_FLUID_SLOT);
     public static final ThemeBuilder<?> PRIMITIVE_THEME = new ThemeBuilder<>(PRIMITIVE_THEME_ID)
-            .background(IThemeApi.PANEL, GTCoreGuiTextures.PRIMITIVE_BACKGROUND)
-            .background(IThemeApi.ITEM_SLOT, GTCoreGuiTextures.PRIMITIVE_ITEM_SLOT)
-            .background(IThemeApi.FLUID_SLOT, GTCoreGuiTextures.PRIMITIVE_FLUID_SLOT);
+            .background(IThemeApi.PANEL, IDs.PRIMITIVE_BACKGROUND)
+            .background(IThemeApi.ITEM_SLOT, IDs.PRIMITIVE_ITEM_SLOT)
+            .background(IThemeApi.FLUID_SLOT, IDs.PRIMITIVE_FLUID_SLOT);
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
