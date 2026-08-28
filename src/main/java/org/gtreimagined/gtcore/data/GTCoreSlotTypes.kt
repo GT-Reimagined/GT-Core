8e8cb2ca2e5e708be1c49821321703612bd5245f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-08-28 06:28:41 -0400

    converted GTCoreSlotTypes to kotlin

--- src/main/java/org/gtreimagined/gtcore/data/GTCoreSlotTypes.kt
+++ src/main/java/org/gtreimagined/gtcore/data/GTCoreSlotTypes.kt
@@ -1,95 +1,137 @@
-package org.gtreimagined.gtcore.data;
+package org.gtreimagined.gtcore.data
 
-import brachy.modularui.widgets.slot.ModularSlot;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.wrapper.EmptyHandler;
-import org.gtreimagined.gtcore.gui.slots.SlotBlueprint;
-import org.gtreimagined.gtcore.gui.slots.SlotCrafting;
-import org.gtreimagined.gtcore.gui.slots.SlotUnlimited;
-import org.gtreimagined.gtcore.gui.slots.SlotCraftingOutput;
-import org.gtreimagined.gtcore.mui.GTCoreGuiTextures;
-import org.gtreimagined.gtlib.blockentity.BlockEntityMachine;
-import org.gtreimagined.gtlib.gui.SlotType;
-import org.gtreimagined.gtlib.gui.slot.AbstractSlot;
-import org.gtreimagined.gtlib.tool.IGTTool;
-import org.gtreimagined.tesseract.TesseractCapUtils;
+import brachy.modularui.widgets.slot.ModularSlot
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraftforge.items.IItemHandlerModifiable
+import net.minecraftforge.items.wrapper.EmptyHandler
+import org.gtreimagined.gtcore.gui.slots.SlotBlueprint
+import org.gtreimagined.gtcore.gui.slots.SlotCrafting
+import org.gtreimagined.gtcore.gui.slots.SlotCraftingOutput
+import org.gtreimagined.gtcore.gui.slots.SlotUnlimited
+import org.gtreimagined.gtcore.mui.GTCoreGuiTextures
+import org.gtreimagined.gtlib.blockentity.BlockEntityMachine
+import org.gtreimagined.gtlib.gui.SlotType
+import org.gtreimagined.gtlib.gui.slot.AbstractSlot
+import org.gtreimagined.gtlib.tool.IGTTool
+import org.gtreimagined.tesseract.TesseractCapUtils
+import org.gtreimagined.tesseract.api.eu.IEnergyHandler
+import org.gtreimagined.tesseract.api.eu.IEnergyHandlerItem
+import java.util.function.BiPredicate
+import java.util.function.Function
 
-public class GTCoreSlotTypes {
-    public static SlotType<AbstractSlot<?>> TOOLS = SlotType.create(b -> {
-        b.setId("tools");
-        b.slotSupplier((type, gui, item, i, d) -> {
-            return new AbstractSlot<>(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-        });
-        b.setTester((t, i) -> i.getItem() instanceof IGTTool || i.getItem().canBeDepleted());
-    });
-    public static SlotType<AbstractSlot<?>> TOOL_CHARGE = SlotType.create(b -> {
-        b.setId("tool_charge");
-        b.slotSupplier((type, gui, item, i, d) -> {
-            return new AbstractSlot<>(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-        });
-        b.setTester((t, i) -> {
-            boolean electric = t instanceof BlockEntity tile && TesseractCapUtils.INSTANCE.getEnergyHandler(tile, null).map(eh -> {
-                return TesseractCapUtils.INSTANCE.getEnergyHandlerItem(i).map(inner -> {
-                    return (inner.getInputVoltage() | inner.getOutputVoltage()) <= (eh.getInputVoltage() | eh.getOutputVoltage());
-                }).orElse(false);
-            }).orElse(false);
-            return electric || i.getItem() instanceof IGTTool || i.getItem().canBeDepleted();
-        });
-        b.setOverlay(GTCoreGuiTextures.ELECTRIC_TOOL_SLOT_OVERLAY);
-    });
-
-    public static SlotType<SlotCrafting> CRAFTING = SlotType.create(b -> {
-        b.setId("crafting");
-        b.setSlotSupplier((type, gui, item, i, d) -> {
-            return new SlotCrafting(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-        });
-        b.setAllowExternalOutput(false);
-    });
-    public static SlotType<AbstractSlot<?>> PARK = SlotType.create(b -> {
-        b.setId("park");
-        b.setSlotSupplier((type, gui, item, i, d) -> {
-            return new AbstractSlot<>(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-        });
-        b.setAllowExternalInput(false);
-        b.allowExternalOutput(false);
-        b.slotGroup(false);
-        b.overlay(GTCoreGuiTextures.PARK_SLOT_OVERLAY);
-    });
-    public static SlotType<SlotBlueprint> BLUEPRINT = SlotType.create(r -> {
-        r.setId("blueprint");
-        r.setSlotSupplier((type, gui, item, i, d) -> {
-                    return new SlotBlueprint(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-                });
-        r.setTester((t, i) -> i.getItem() == GTCoreItems.Blueprint || i.getItem() == GTCoreItems.EmptyBlueprint);
-        r.setAllowExternalOutput(false);
-        r.setOverlay(GTCoreGuiTextures.BLUEPRINT_SLOT_OVERLAY);
-    });
-    public static SlotType<AbstractSlot<?>> EXPORT = SlotType.create(b -> {
-        b.setId("export");
-        b.setSlotSupplier((type, gui, item, i, d) -> new AbstractSlot<>(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setSlotGroup(false);
-        b.setOverlay(GTCoreGuiTextures.EXPORT_SLOT_OVERLAY);
-    });
-    public static SlotType<ModularSlot> CRAFTING_RESULT = SlotType.create(b -> {
-        b.setId("crafting_result");
-        b.setSlotSupplier((type, tile, slots, index, data) -> {
-            if (tile instanceof BlockEntityMachine<?> machine) {
-                return new SlotCraftingOutput(machine.itemHandler.map(m -> m).orElse(null), slots.getOrDefault(type, EmptyHandler.INSTANCE), (IItemHandlerModifiable) slots.getOrDefault(CRAFTING, new EmptyHandler()), index);
+object GTCoreSlotTypes {
+    @JvmField
+    val TOOLS: SlotType<AbstractSlot<*>> = SlotType.create { b ->
+            b.id = "tools"
+            b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                AbstractSlot(type, gui,
+                    item.getOrDefault(type, EmptyHandler.INSTANCE), i)
             }
-            return new ModularSlot(slots.getOrDefault(type, EmptyHandler.INSTANCE), index);
-        });
-        b.setAllowExternalInput(false);
-        b.setOverlay(GTCoreGuiTextures.CRAFTING_OUTPUT_SLOT_OVERLAY);
-    });
+            b.tester = BiPredicate { _, i ->
+                i.item is IGTTool || i.item.canBeDepleted()
+            }
+        }
 
-    public static SlotType<SlotUnlimited> UNLIMITED = SlotType.create(b -> {
-        b.setId("unlimited");
-        b.setSlotSupplier((type, gui, item, i, d) -> {
-                    return new SlotUnlimited(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i);
-                });
-    });
-    public static void init(){
+    @JvmField
+    val TOOL_CHARGE: SlotType<AbstractSlot<*>> = SlotType.create { b ->
+            b.id = "tool_charge"
+            b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                AbstractSlot(type, gui,
+                    item.getOrDefault(type, EmptyHandler.INSTANCE), i)
+            }
+            b.tester = BiPredicate { t, i ->
+                val electric = t is BlockEntity && TesseractCapUtils.INSTANCE.getEnergyHandler(t, null).map(
+                    Function { eh: IEnergyHandler ->
+                        TesseractCapUtils.INSTANCE.getEnergyHandlerItem(i)
+                            .map(Function { inner: IEnergyHandlerItem? -> (inner!!.inputVoltage or inner.outputVoltage) <= (eh.inputVoltage or eh.outputVoltage) })
+                            .orElse(false)!!
+                    }).orElse(false)!!
+                electric || i.item is IGTTool || i.item.canBeDepleted()
+            }
+            b.overlay = GTCoreGuiTextures.ELECTRIC_TOOL_SLOT_OVERLAY
+        }
 
+    @JvmField
+    val CRAFTING: SlotType<SlotCrafting> = SlotType.create { b ->
+            b.id = "crafting"
+            b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                    SlotCrafting(
+                        type,
+                        gui,
+                        item.getOrDefault(type, EmptyHandler.INSTANCE),
+                        i
+                    )
+                }
+            b.allowExternalOutput = false
+        }
+
+    @JvmField
+    val PARK: SlotType<AbstractSlot<*>> = SlotType.create { b ->
+            b.id = "park"
+            b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                    AbstractSlot(type, gui,
+                        item.getOrDefault(type, EmptyHandler.INSTANCE), i)
+                }
+            b.allowExternalInput = false
+            b.allowExternalOutput(false)
+            b.slotGroup(false)
+            b.overlay(GTCoreGuiTextures.PARK_SLOT_OVERLAY)
+        }
+
+    @JvmField
+    val BLUEPRINT: SlotType<SlotBlueprint> = SlotType.create { r ->
+            r.id = "blueprint"
+            r.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                    SlotBlueprint(type, gui,
+                        item.getOrDefault(type, EmptyHandler.INSTANCE), i)
+                }
+            r.tester = BiPredicate { _, i -> i.item === GTCoreItems.Blueprint || i.item === GTCoreItems.EmptyBlueprint }
+            r.allowExternalOutput = false
+            r.overlay = GTCoreGuiTextures.BLUEPRINT_SLOT_OVERLAY
+        }
+
+    @JvmField
+    val EXPORT: SlotType<AbstractSlot<*>> = SlotType.create { b ->
+            b.id = "export"
+            b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                    AbstractSlot(type, gui,
+                        item.getOrDefault(type, EmptyHandler.INSTANCE), i)
+                }
+            b.slotGroup = false
+            b.overlay = GTCoreGuiTextures.EXPORT_SLOT_OVERLAY
+        }
+
+    @JvmField
+    val CRAFTING_RESULT: SlotType<ModularSlot> = SlotType.create { b ->
+            b.id = "crafting_result"
+            b.slotSupplier = SlotType.ISlotSupplier { type, tile, slots, index, _ ->
+                    if (tile is BlockEntityMachine<*>) {
+                        return@ISlotSupplier SlotCraftingOutput(
+                            tile.itemHandler.map(Function { m -> m }).orElse(null),
+                            slots.getOrDefault(type, EmptyHandler.INSTANCE),
+                            slots.getOrDefault(
+                                CRAFTING, EmptyHandler.INSTANCE
+                            ) as IItemHandlerModifiable,
+                            index
+                        )
+                    }
+                    ModularSlot(slots.getOrDefault(type, EmptyHandler.INSTANCE), index)
+                }
+            b.allowExternalInput = false
+            b.overlay = GTCoreGuiTextures.CRAFTING_OUTPUT_SLOT_OVERLAY
+        }
+
+    @JvmField
+    val UNLIMITED: SlotType<SlotUnlimited> = SlotType.create { b ->
+            b.id = "unlimited"
+            b.slotSupplier =
+                SlotType.ISlotSupplier { type, gui, item, i, _ ->
+                    SlotUnlimited(type, gui,
+                        item.getOrDefault(type, EmptyHandler.INSTANCE), i)
+                }
+        }
+
+    @JvmStatic
+    fun init() {
     }
 }
